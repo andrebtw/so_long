090e357cbe780bc8c53cfb00ecbe6bde521ee9a7
André <gwandrewgd@gmail.com>
2023-01-12 14:00:42 +0100

    Made pathfinding complete, SEGFAULT somewhere.

--- src/map_gestion/map_path_check.c
+++ src/map_gestion/map_path_check.c
@@ -6,7 +6,7 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 16:20:28 by anrodri2          #+#    #+#             */
-/*   Updated: 2022/12/21 17:28:51 by anrodri2         ###   ########.fr       */
+/*   Updated: 2023/01/12 13:59:36 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,21 +79,35 @@ int	findpath(t_map_info map, char **path, int *end_pos)
 	return (0);
 }
 
-void print_map(char **map)
+int	path_check_loop(char *file, int **c_coords, t_map_info mapi)
 {
-	int y = 0; 
-	int x = 0;
+	int		i;
+	int		not_end;
+	char	**map_copy;
 
-	while (map[y])
+	i = 0;
+	not_end = 1;
+	while (c_coords[i] && not_end)
 	{
-		x = 0;
-		while (map[y][x])
+		if (!c_coords[i])
+			not_end = 0;
+		map_copy = map_opening(file);
+		if (!map_copy)
+			return (MALLOC_ERROR);
+		if (not_end)
+			if (findpath(mapi, map_copy, c_coords[i]) != 1)
+				return (tab_free(map_copy), tab_free_int(c_coords), ERROR);
+		if (!not_end)
 		{
-			ft_printf("%c", map[y][x]);
-			x++;
+			c_coords[0][0] = mapi.e_index_y;
+			c_coords[0][1] = mapi.e_index_x;
+			if (findpath(mapi, map_copy, c_coords[0]) != 1)
+				return (tab_free(map_copy), tab_free_int(c_coords), ERROR);
 		}
-		y++;
+		tab_free(map_copy);
+		i++;
 	}
+	return (0);
 }
 
 int	path_check(char **map, char *file)
@@ -101,7 +115,6 @@ int	path_check(char **map, char *file)
 	t_map_info	mapi;
 	t_map		mapc;
 	int			**c_coords;
-	char		**map_copy;
 
 	mapc.x = 0;
 	mapc.y = 0;
@@ -109,25 +122,8 @@ int	path_check(char **map, char *file)
 	mapi = find_e_index(map, mapc, mapi);
 	mapi = find_p_index(map, mapc, mapi);
 	c_coords = get_collectibles(map);
-	int i = 0;
-	while (c_coords[i])
-	{
-		map_copy = map_opening(file);
-		if (!map_copy)
-			return (MALLOC_ERROR);
-		if (findpath(mapi, map_copy, c_coords[i]) != 1)
-		{
-			//ft_printf("ERROR OF PATH LOL");
-			print_map(map_copy);
-			tab_free(map_copy);
-			return (tab_free_int(c_coords), ERROR);
-		}
-		print_map(map_copy);
-		tab_free(map_copy);
-		//ft_printf("y : %d ; x : %d\n", c_coords[i][0], c_coords[i][1]);
-		i++;
-	}
-	ft_printf("C count : %d\n", mapi.c_count);
+	if (path_check_loop(file, c_coords, mapi) != 1)
+		return (tab_free_int(c_coords), ERROR);
 	tab_free_int(c_coords);
 	return (0);
 }
