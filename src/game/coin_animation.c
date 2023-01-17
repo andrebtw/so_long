338a494c91fca6fd98d88723bd44c36dea293299
André <gwandrewgd@gmail.com>
2023-01-17 13:26:02 +0100

    Animation now works.

--- src/game/coin_animation.c
+++ src/game/coin_animation.c
@@ -6,13 +6,13 @@
 /*   By: anrodri2 <anrodri2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 14:33:05 by anrodri2          #+#    #+#             */
-/*   Updated: 2023/01/16 17:04:28 by anrodri2         ###   ########.fr       */
+/*   Updated: 2023/01/17 13:25:17 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header.h"
 
-void	draw_frame_loop(t_mlx *mlx, t_map mapc, t_map coords)
+void	draw_frame_loop(t_mlx *mlx, t_map mapc, t_map coords, t_xmp *grass)
 {
 	while (mlx->map[mapc.y])
 	{
@@ -21,8 +21,12 @@ void	draw_frame_loop(t_mlx *mlx, t_map mapc, t_map coords)
 		while (mlx->map[mapc.y][++mapc.x])
 		{
 			if (ft_strchr("C", mlx->map[mapc.y][mapc.x]))
+			{
+				mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr,
+					grass->ptr, coords.x, coords.y);
 				mlx_put_image_to_window(mlx->mlx_ptr, mlx->win_ptr,
 					mlx->animation.coin.ptr, coords.x, coords.y);
+			}
 			coords.x = coords.x + 50;
 		}
 		mapc.y++;
@@ -34,15 +38,18 @@ int	draw_frame(t_mlx *mlx, char *location)
 {
 	t_map	mapc;
 	t_map	coords;
+	t_xmp	grass;
 
 	coords.x = 0;
 	coords.y = 0;
 	mapc.y = 0;
 	mlx->animation.coin.ptr = mlx_xpm_file_to_image(mlx->mlx_ptr, location,
 			&mlx->animation.coin.width, &mlx->animation.coin.height);
-	if (!mlx->animation.coin.ptr)
+	grass.ptr = mlx_xpm_file_to_image(mlx->mlx_ptr, "./assets/grass.xpm",
+		&grass.width, &grass.height);
+	if (!mlx->animation.coin.ptr || !grass.ptr)
 		return (ERROR);
-	draw_frame_loop(mlx, mapc, coords);
+	draw_frame_loop(mlx, mapc, coords, &grass);
 	return (0);
 }
 
